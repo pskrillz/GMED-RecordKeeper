4928c80191db673cd8ccee832fc24a9ee1b7ad8b
pi <payam.farah@live.com>
2020-08-27 17:29:43 -0700

    added sample functionality of table working

--- src/app/features/resourceLibrary/table-view/table-view.component.ts
+++ src/app/features/resourceLibrary/table-view/table-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TableDataService } from '../../../table-data.service'
 
 @Component({
   selector: 'app-table-view',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _http: HttpClient, public _tableData: TableDataService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this._tableData.getTableData().subscribe(
+      (res) =>{
+        console.log("getTableData() ran on init", res )
+        this.tableData = res
+      }
+    )
   }
+
+formData= {
+  Name: null,
+  Owner: null,
+}
+
+
+tableData: any = [];
+
+
+// users = [
+//   {
+//   id:"1",
+//   name:"paul", 
+//   date:"1/14/20",
+//   color:"blue",
+// }
+// ]
+
+
+addEntry(formData){
+this._tableData.addEntry(formData).subscribe(
+  (res) => {
+  console.log("dataworked", "formData: " + formData, res)
+  }
+)
+}
+
+
+
 
 }
