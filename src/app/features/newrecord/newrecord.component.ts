d41f1056f900c4153498f478116d4f0c50172796
pi <payam.farah@live.com>
2020-11-14 00:15:28 -0800

    working on stepper for modal and implementing column view

--- src/app/features/newrecord/newrecord.component.ts
+++ src/app/features/newrecord/newrecord.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TableViewComponent } from "../resourceLibrary/table-view/table-view.component"
 import { TableDataService } from '../../table-data.service'
+import
 
 @Component({
   selector: 'app-newrecord',
@@ -19,7 +20,13 @@ export class NewrecordComponent implements OnInit {
 
 
   clearForms(){
-    
+    this._tableView.formData.version = "";
+    this._tableView.formData.reqBy = "";
+    this._tableView.formData.reqDate = "";
+    this._tableView.formData.dep = "";
+    this._tableView.formData.name = "";
+    this._tableView.formData.subject = "";
+    this._tableView.formData.description = "";
   }
 
 
